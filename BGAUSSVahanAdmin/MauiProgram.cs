f3dc6b233c4a93b4b5d949b7cbf1722b7105da92
Priyanka Nikam <priyanka.nikam@bgauss.com>
2026-02-23 17:12:34 +0530

    #For window authentication changed MSAL client with Tenant-specific authority

--- BGAUSSVahanAdmin/MauiProgram.cs
+++ BGAUSSVahanAdmin/MauiProgram.cs
@@ -25,10 +25,20 @@ public static class MauiProgram
 
         // ✅ Initialize MSAL client with Tenant-specific authority
         PCA = PublicClientApplicationBuilder
-    .Create("30c02e55-3684-45cf-b5f7-65d4251e12d2")
-    .WithAuthority("https://login.microsoftonline.com/a265301a-63b1-4aec-9d47-273b49c178b4")
-    .WithRedirectUri("msauth://com.companyname.bgaussvahanadmin/RkPW28YqMFq30noUKxVIWoAox9w%3D")
-    .Build();
+     .Create("30c02e55-3684-45cf-b5f7-65d4251e12d2")
+     .WithAuthority("https://login.microsoftonline.com/a265301a-63b1-4aec-9d47-273b49c178b4")
+#if WINDOWS
+    .WithRedirectUri("http://localhost")
+#else
+     .WithRedirectUri("msauth://com.companyname.bgaussvahanadmin/RkPW28YqMFq30noUKxVIWoAox9w%3D")
+#endif
+     .Build();
+
+    //    PCA = PublicClientApplicationBuilder
+    //.Create("30c02e55-3684-45cf-b5f7-65d4251e12d2")
+    //.WithAuthority("https://login.microsoftonline.com/a265301a-63b1-4aec-9d47-273b49c178b4")
+    //.WithRedirectUri("msauth://com.companyname.bgaussvahanadmin/RkPW28YqMFq30noUKxVIWoAox9w%3D")
+    //.Build();
 
         return builder.Build();
     }
